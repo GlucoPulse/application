3375112bff015e1eebc0df747d4ae991a35e0660
clerizedd <casucolerizedoreen+github@gmail.com>
2024-08-11 23:34:12 +0800

    db

--- App.js
+++ App.js
@@ -1,5 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SQLiteProvider, useSQLiteContext } from 'expo-sqlite';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const initializeDatabase = async (db) => {
+  try {
+    await db.execAsync(`
+      PRAGMA journal_mode = WAL;
+      CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+      );
+
+      `);
+      console.log ("DATABASE INITIALIZED")
+  }
+  catch (error) {
+    console.log ("Error while initializing the database: ", error);
+  }
+}
+
+
 
 export default function App() {
   return (
