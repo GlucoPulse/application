3622e8cb9a2838af3934ef9dcbf9fbf5a0376f99
clerizedd <casucolerizedoreen+github@gmail.com>
2024-09-13 21:49:18 +0800

    -

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,29 @@
 import { KeyboardAvoidingView, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
+import { auth } from '../firebase';
+import firebaseConfig from '../firebase'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+
 
 const LoginScreen = () => {
+    const [email, setEmail] = useState ('');
     const [username, setUsername] = useState ('');
     const [password, setPassword] = useState ('');
+
+    const handleSignUp = () => {
+        createUserWithEmailAndPassword (auth, email, password)
+            .then ((userCredentials) => {
+                const user = userCredentials.user;
+                console.log (user.username);
+            })
+            .catch((error) => {alert(error.message)}); //WEAK PASSWORD AND USED EMAIL
+    }
 
     return (
         <KeyboardAvoidingView style = {styles.container} behavior = 'padding'>
             <View style={styles.inputContainer}>
+                <TextInput placeholder = "Email" style = {styles.input} value={email} onChangeText={text => setEmail(text)} /> 
                 <TextInput placeholder = "Username" style = {styles.input} value={username} onChangeText={text => setUsername(text)} /> 
                 <TextInput placeholder = "Password" style = {styles.input} secureTextEntry value={password} onChangeText={text => setPassword(text)} />
             </View>
@@ -18,7 +33,7 @@ const LoginScreen = () => {
                     <Text style ={styles.buttonText}> Login </Text>
                 </TouchableOpacity>
 
-                <TouchableOpacity onPress= {() => {}} style={[styles.button, styles.buttonOutline]}>
+                <TouchableOpacity onPress= {handleSignUp} style={[styles.button, styles.buttonOutline]}>
                     <Text style ={styles.buttonOutlineText}> Register </Text>
                 </TouchableOpacity>
             </View>
